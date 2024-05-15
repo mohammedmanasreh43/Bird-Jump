3885d4509472e2a34c27c1d964bbf37e666f1f3e
mohammedmanasreh43 <mohammedmanasreh45@gmail.com>
2024-05-15 21:30:58 +0300

    10

---
+++ Assets/Scripts/UI Scripts/LoadSc.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DentedPixel;
+using UnityEngine.SceneManagement;
+
+public class LoadSc : MonoBehaviour
+{
+    public GameObject load;
+    public float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimateBar();
+        Invoke("LoadNextScene", 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AnimateBar()
+    {
+        LeanTween.scaleX(load,1,time);
+    }
+    void LoadNextScene()
+    {
+        // Load the next scene by name
+        SceneManager.LoadScene("MinMune");
+    }
+}
